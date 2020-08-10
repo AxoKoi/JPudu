2c906b61a786b27dda289067e7278a33b8a0ade9
Ignacio Rondini <jirondini@gmail.com>
2020-08-10 06:39:10 +0200

    Add header note.

--- src/test/java/com/axokoi/png/chunk/IHDRChunkBuilderTest.java
+++ src/test/java/com/axokoi/png/chunk/IHDRChunkBuilderTest.java
@@ -1,3 +1,7 @@
+/*
+This is a file belonging to an axokoi project. The source code has been released under a MPL 2.0 license.
+For more information, visit www.axokoi.com or www.github.com/axokoi
+*/
 package com.axokoi.png.chunk;
 
 import java.nio.ByteBuffer;
